24ee6c9df2aef566f180729265c43fbb3613c916
mariamelboudi <121954434+mariamelboudi@users.noreply.github.com>
2025-03-10 20:02:31 +0100

    Update 001_SQL_Murder_Mysteries.sql

--- queries/001_SQL_Murder_Mysteries/001_SQL_Murder_Mysteries.sql
+++ queries/001_SQL_Murder_Mysteries/001_SQL_Murder_Mysteries.sql
@@ -17,3 +17,32 @@ LIMIT 1;
 SELECT *  
 FROM person  
 WHERE name LIKE '%Annabel%' AND address_street_name = 'Franklin Ave'; 
+
+-- Retrieve transcripts
+SELECT interview.person_id, person.name, interview.transcript 
+FROM interview
+LEFT JOIN person ON interview.person_id = person.id
+WHERE name = 'Morty Schapiro' OR name = 'Annabel Miller'
+
+-- Retrieve gym members with a gold membership who were at the gym on January 9th
+SELECT get_fit_now_check_in.check_in_date, get_fit_now_member.name
+FROM get_fit_now_member
+LEFT JOIN get_fit_now_check_in ON get_fit_now_member.id = get_fit_now_check_in.membership_id
+WHERE check_in_date = 20180109 AND membership_status = 'gold';
+
+-- Connect these gym members to a driver's license and return with date and plate number as conditions
+SELECT 
+    get_fit_now_check_in.check_in_date, 
+    get_fit_now_member.name, 
+    drivers_license.plate_number 
+FROM get_fit_now_member
+LEFT JOIN get_fit_now_check_in ON get_fit_now_member.id = get_fit_now_check_in.membership_id
+LEFT JOIN person ON get_fit_now_member.person_id = person.id
+LEFT JOIN drivers_license ON person.license_id = drivers_license.id
+WHERE check_in_date = 20180109 AND plate_number LIKE '%H42W%';
+
+-- Retrieve the interview transcript for Jeremy Bowers
+SELECT person.name, interview.transcript
+FROM person
+LEFT JOIN interview ON person.id = interview.person_id
+WHERE name = 'Jeremy Bowers'
